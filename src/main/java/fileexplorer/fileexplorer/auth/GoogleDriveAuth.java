99f1d86651c08b92c4b5f16a87aa5e1e3712fdff
Simon István <h163789@stud.u-szeged.hu>
2026-05-10 16:45:20 +0200

    GoogleDriveAuth javítása

--- src/main/java/fileexplorer/fileexplorer/auth/GoogleDriveAuth.java
+++ src/main/java/fileexplorer/fileexplorer/auth/GoogleDriveAuth.java
@@ -30,7 +30,7 @@ public class GoogleDriveAuth {
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         InputStream in = GoogleDriveAuth.class.getResourceAsStream("/credentials.json");
         if (in == null) {
-            throw new FileNotFoundException("Unable to load credentials file.");
+            throw new FileNotFoundException("Nincs elérhető token.");
         }
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
@@ -49,27 +49,5 @@ public class GoogleDriveAuth {
         return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-    }
-
-    public static void deleteUserToken(String userId) {
-        try {
-            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-
-            // Ugyanazt a flow-t kell felépítenünk, mint a bejelentkezésnél
-            InputStream in = GoogleDriveAuth.class.getResourceAsStream("/credentials.json");
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                    .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                    .setAccessType("offline")
-                    .build();
-
-            flow.getCredentialDataStore().delete(userId);
-
-            System.out.println("Kijelentkezés: " + userId + " tokenje törölve.");
-        } catch (Exception e) {
-            System.err.println("Hiba a specifikus kijelentkezéskor: " + e.getMessage());
-        }
     }
 }
